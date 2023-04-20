43c508520bc1b50aa89e97835320a49712492305
hzy1721 <hzy1721@qq.com>
2023-04-20 15:45:57 +0800

    ci: 部署脚本

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,15 @@
-import { fileURLToPath, URL } from "url";
+import { fileURLToPath, URL } from 'url';
 
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
+export default defineConfig(() => {
   return {
-    base: command === 'build' ? '/douban/' : '/',
     plugins: [vue()],
     resolve: {
       alias: {
-        "@": fileURLToPath(new URL("./src", import.meta.url)),
+        '@': fileURLToPath(new URL('./src', import.meta.url)),
       },
     },
   };
